c307fd56ea11478dff7ea824ffae4918241e3c12
xiaoyifan <xiaoyifan@uchicago.edu>
2016-04-25 04:21:23 -0700

    widget is implemented.

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/widgetRemoteService.java
@@ -0,0 +1,133 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+
+/**
+ * Created by Yifan on 4/25/16.
+ */
+public class widgetRemoteService extends RemoteViewsService {
+
+    public final String LOG_TAG = widgetRemoteService.class.getSimpleName();
+
+    private static final String[] STOCK_COLUMNS = {
+            QuoteColumns._ID,
+            QuoteColumns.SYMBOL,
+            QuoteColumns.BIDPRICE,
+            QuoteColumns.PERCENT_CHANGE,
+    };
+    // these indices must match the projection
+    static final int INDEX_QUOTE_ID = 0;
+    static final int INDEX_QUOTE_SYMBOL = 1;
+    static final int INDEX_QUOTE_BIDPRICE = 2;
+    static final int INDEX_QUOTE_PERCENT_CHANGE = 3;
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RemoteViewsFactory() {
+            private Cursor data = null;
+
+            @Override
+            public void onCreate() {
+                // Nothing to do
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data != null) {
+                    data.close();
+                }
+                // This method is called by the app hosting the widget (e.g., the launcher)
+                // However, our ContentProvider is not exported so it doesn't have access to the
+                // data. Therefore we need to clear (and finally restore) the calling identity so
+                // that calls use our process and permission
+                final long identityToken = Binder.clearCallingIdentity();
+                Uri weatherForLocationUri = QuoteProvider.Quotes.CONTENT_URI;
+
+                data = getContentResolver().query(weatherForLocationUri,
+                        STOCK_COLUMNS,
+                        QuoteColumns.ISCURRENT + " = ?",
+                        new String[]{"1"},
+                        null);
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION ||
+                        data == null || !data.moveToPosition(position)) {
+                    return null;
+                }
+                RemoteViews views = new RemoteViews(getPackageName(),
+                        R.layout.widget_detail_list_item);
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                    setRemoteContentDescription(views, data.getString(INDEX_QUOTE_SYMBOL));
+                }
+                views.setTextViewText(R.id.stock_symbol, data.getString(INDEX_QUOTE_SYMBOL));
+                views.setTextViewText(R.id.bid_price, data.getString(INDEX_QUOTE_BIDPRICE));
+                views.setTextViewText(R.id.change, data.getString(INDEX_QUOTE_PERCENT_CHANGE));
+
+                final Intent fillInIntent = new Intent();
+
+                Uri quoteUri = QuoteProvider.Quotes.CONTENT_URI;
+                fillInIntent.setData(quoteUri);
+                fillInIntent.putExtra("symbol",data.getString(INDEX_QUOTE_SYMBOL));
+                views.setOnClickFillInIntent(R.id.widget_item, fillInIntent);
+                return views;
+            }
+
+            @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+            private void setRemoteContentDescription(RemoteViews views, String description) {
+                views.setContentDescription(R.id.stock_symbol, description);
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.widget_detail_list_item);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (data.moveToPosition(position))
+                    return data.getLong(INDEX_QUOTE_ID);
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+}
